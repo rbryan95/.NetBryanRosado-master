52a20289ef11b547e50dfa419d956973fb045b6f
rbryan95 <berd1495@gmail.com>
2023-10-17 00:58:32 -0500

    Add Ticket Manager Class Fix main program to call new class

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace TicketSystemApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Add Ticket");
+                Console.WriteLine("2. Quit");
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    TicketManager.AddTicket();
+                }
+                else if (choice == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please select a valid option.");
+                }
+            }
+        }
+    }
+}
+
---
+++ TicketManager.cs
@@ -0,0 +1,28 @@
+ class TicketManager
+    {
+        public static void AddTicket()
+        {
+            Console.Write("Enter TicketID: ");
+            string ticketID = Console.ReadLine();
+            Console.Write("Enter Summary: ");
+            string summary = Console.ReadLine();
+            Console.Write("Enter Status: ");
+            string status = Console.ReadLine();
+            Console.Write("Enter Priority: ");
+            string priority = Console.ReadLine();
+            Console.Write("Enter Submitter: ");
+            string submitter = Console.ReadLine();
+            Console.Write("Enter Assigned: ");
+
+            // Process the ticket data here
+        }
+    }
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TicketManager.AddTicket();
+    }
+}
