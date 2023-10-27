443e50fed14e654122f5bedef4440e58c13e744a
rbryan95 <berd1495@gmail.com>
2023-10-26 20:33:43 -0500

    Add TaskTicket in Ticket class

--- Ticket.cs
+++ Ticket.cs
@@ -50,3 +50,15 @@ using System.IO;
         }
     }
 
+    class TaskTicket : Ticket
+    {
+        public string ProjectName { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public TaskTicket(string ticketID, string summary, string status, string priority, string submitter, string assigned, string watching, string projectName, DateTime dueDate)
+            : base(ticketID, summary, status, priority, submitter, assigned, watching)
+        {
+            ProjectName = projectName;
+            DueDate = dueDate;
+        }
+    }
