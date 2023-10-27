ce60f679dbc25517dda2aaf8707bb9d4142fd4f8
rbryan95 <berd1495@gmail.com>
2023-10-26 20:33:24 -0500

    Add EnhancementTicket Class in Ticket class

--- Ticket.cs
+++ Ticket.cs
@@ -33,4 +33,20 @@ using System.IO;
         }
     }
 
+    class EnhancementTicket : Ticket
+    {
+        public string Software { get; set; }
+        public double Cost { get; set; }
+        public string Reason { get; set;}
+        public double Estimate { get; set; }
+
+        public EnhancementTicket(string ticketID, string summary, string status, string priority, string submitter, string assigned, string watching, string software, double cost, string reason, double estimate)
+            : base(ticketID, summary, status, priority, submitter, assigned, watching)
+        {
+            Software = software;
+            Cost = cost;
+            Reason = reason;
+            Estimate = estimate;
+        }
+    }
 
