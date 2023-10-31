b4959e55360dfbb9fa46c1179a386203c30f31b5
rbryan95 <berd1495@gmail.com>
2023-10-31 11:58:06 -0500

    Adding Linq to Main program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace TicketSystemApp
 {
